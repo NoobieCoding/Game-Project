537979ea5527b2dbbe721e73761e0afd0d067933
nuttapong rojanavanich <nuttapong rojanavanich>
2016-03-01 09:25:51 +0700

    empty

---
+++ src/GameLayer.js
@@ -0,0 +1,22 @@
+var GameLayer = cc.LayerColor.extend({
+  init: function() {
+    this._super(new cc.Color(127, 127, 127, 255));
+    this.setPosition(new cc.Point( 0, 0 ));
+
+    this.player = new Player();
+    this.player.setPosition(new cc.Point(screenWidth / 2, screenHeight / 2));
+    this.addChild(this.player);
+    this.player.scheduleUpdate();
+
+    return true;
+  }
+});
+
+var StartScene = cc.Scene.extend({
+  onEnter: function() {
+    this._super();
+    var layer = new GameLayer();
+    layer.init();
+    this.addChild( layer );
+  }
+});
