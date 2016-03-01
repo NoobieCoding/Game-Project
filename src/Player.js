5bc524f7d5af9adb0fa10a28e4afc3a5eed844bd
nuttapong rojanavanich <nuttapong rojanavanich>
2016-03-01 09:52:10 +0700

    with falling function

--- src/Player.js
+++ src/Player.js
@@ -2,5 +2,14 @@ var Player = cc.Sprite.extend({
   ctor: function() {
     this._super();
     this.initWithFile('res/images/dot.png');
+    this.vy = Player.STARTING_VELOCITY;
+  },
+
+  update: function(dt) {
+  var pos = this.getPosition();
+  this.setPosition(new cc.Point(pos.x, pos.y + this.vy));
+  this.vy += Player.G;
   }
 });
+Player.G = -1;
+Player.STARTING_VELOCITY = 15;
